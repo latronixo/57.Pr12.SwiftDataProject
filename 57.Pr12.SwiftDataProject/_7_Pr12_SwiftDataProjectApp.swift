c3d21c4b1f2c11c0fab86fbff2c149e4c0b88e28
Valentin <latronixo@gmail.com>
2025-07-07 18:06:19 +0300

    Initial Commit

---
+++ 57.Pr12.SwiftDataProject/_7_Pr12_SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_Pr12_SwiftDataProjectApp.swift
+//  57.Pr12.SwiftDataProject
+//
+//  Created by Валентин on 07.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct _7_Pr12_SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
