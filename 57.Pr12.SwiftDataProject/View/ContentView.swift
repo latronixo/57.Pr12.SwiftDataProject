d642b93fe5c4d4feb93f8d24f8cadc16128821ca
Valentin <latronixo@gmail.com>
2025-07-14 21:55:01 +0300

    57.2/5 Фильтрация по 2 параметрам при помощи if

--- 57.Pr12.SwiftDataProject/View/ContentView.swift
+++ 57.Pr12.SwiftDataProject/View/ContentView.swift
@@ -11,8 +11,15 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query(filter: #Predicate<User> { user in
-        user.name.localizedStandardContains("R") &&
-        user.city == "London"
+        if user.name.localizedStandardContains("R") {
+            if user.city == "London" {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
     }, sort: \User.name) var users: [User]
     @State private var path = [User]()
     
