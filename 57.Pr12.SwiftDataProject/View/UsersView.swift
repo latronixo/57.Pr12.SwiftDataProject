3537af4953ee30eb59b09ccf78485ac7cc4624bc
Valentin <latronixo@gmail.com>
2025-07-15 17:15:07 +0300

    58.4/5 Добавил добавление пользователя в БД и пару задач для него при появлении формы

--- 57.Pr12.SwiftDataProject/View/UsersView.swift
+++ 57.Pr12.SwiftDataProject/View/UsersView.swift
@@ -9,7 +9,9 @@ import SwiftData
 import SwiftUI
 
 struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
+    
     var body: some View {
         List(users) { user in
             HStack {
@@ -26,6 +28,7 @@ struct UsersView: View {
                     .clipShape(.capsule)
             }
         }
+        .onAppear(perform: addSample)
     }
     
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
@@ -33,6 +36,18 @@ struct UsersView: View {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        let user1 = User(name: "Петр Ильич", city: "Москва", joinDate: .now)
+        let job1 = Job(name: "Организовать уборку носков", priority: 3)
+        let job2 = Job(name: "Встретиться с Алексом", priority: 4)
+        
+        modelContext.insert(user1)  //добавим пользователя в БД
+        
+        //добавим ему задачи
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
